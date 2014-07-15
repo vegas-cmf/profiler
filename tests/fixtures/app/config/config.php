a2d77f6c4f95f468144dd664415e7dfc01be1dc3
slawek <slawomir.zytko@gmail.com>
2014-07-15 12:58:44 +0200

    Core update

--- tests/fixtures/app/config/config.php
+++ tests/fixtures/app/config/config.php
@@ -3,23 +3,19 @@ if (!defined('APP_ROOT')) define('APP_ROOT', dirname(dirname(__DIR__)));
 
 return array(
     'application' => array(
-        'servicesDir'   =>  APP_ROOT . '/app/services/',
+        'environment'    => \Vegas\Constants::DEV_ENV,
+        'serviceDir'   =>  APP_ROOT . '/app/services/',
         'configDir'     => dirname(__FILE__) . DIRECTORY_SEPARATOR,
         'libraryDir'     => dirname(APP_ROOT) . DIRECTORY_SEPARATOR,
         'pluginDir'      => APP_ROOT . '/app/plugins/',
-        'moduleDir'      => APP_ROOT . '/app/module/',
+        'moduleDir'      => APP_ROOT . '/app/modules/',
         'baseUri'        => '/',
         'language'       => 'nl_NL',
-        'subModules'    =>  array(
-            'frontend', 'backend', 'custom'
-        ),
         'view'  => array(
             'cacheDir'  =>  APP_ROOT . '/cache/',
             'layout'    =>  'main',
             'layoutsDir'    =>  APP_ROOT . '/app/layouts/',
         ),
-
-        'environment'    => 'development'
     ),
 
     'plugins' => array(),
